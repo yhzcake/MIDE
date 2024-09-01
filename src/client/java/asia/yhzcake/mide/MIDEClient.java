81c71284236c5b3b03a6ce594e9fa5269d87c367
yhz_cake@zhimaoheli.onmicrosoft.com <8619207+yhz_cake@user.noreply.gitee.com>
2024-09-01 13:34:11 +0800

    init

---
+++ src/client/java/asia/yhzcake/mide/MIDEClient.java
@@ -0,0 +1,10 @@
+package asia.yhzcake.mide;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MIDEClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
